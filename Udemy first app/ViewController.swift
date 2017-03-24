01e1954cf427c83c5d4d3e1932e09a18757939fb
Devon Lloyd <devonlloyd@fb.com>
2017-03-24 13:07:52 -0500

    add files for Udemy first project

--- Udemy first app/ViewController.swift
+++ Udemy first app/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var CoolLogo: UIImageView!
+    @IBOutlet weak var CoolBG: UIImageView!
+    @IBOutlet weak var UncoolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MakeMeNotSoUncool(_ sender: Any) {
+        CoolLogo.isHidden = false
+        CoolBG.isHidden = false
+        UncoolButton.isHidden = true
+    }
 
 }
 
